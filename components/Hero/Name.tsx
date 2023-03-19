42b76e2ed6201a64673545f27d2d7c5ed1d6154d
Leandro Bernardi <leandro.88.bernardi@gmail.com>
2023-03-18 22:36:41 -0300

    Update Name.tsx
    change hero name

--- components/Hero/Name.tsx
+++ components/Hero/Name.tsx
@@ -1,3 +1,3 @@
 export function Name() {
-    return <p className="text-center text-4xl font-black text-brand-primary md:text-5xl">Leandro Bernardi</p>
+    return <p className="text-center text-6xl font-black text-brand-primary md:text-5xl">Leandro Bernardi</p>
 }
