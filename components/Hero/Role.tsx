b94d84083f7afd856ec0fc0a621910ad544beb52
lokuhra <leandro.88.bernardi@gmail.com>
2023-03-20 11:41:32 -0300

    fix hero texts size

--- components/Hero/Role.tsx
+++ components/Hero/Role.tsx
@@ -1,3 +1,3 @@
 export function Role() {
-    return <p className="text-center text-3xl font-bold text-brand-text md:text-6xl">Frontend Developer</p>
+    return <p className="text-center text-3xl font-bold text-brand-text md:text-5xl">Frontend Developer</p>
 }
